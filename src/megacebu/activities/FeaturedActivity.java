c60c59d34b2a057e22c39396c126a93b43063b66
Farrah Marie Caberte <pangcaberte@gmail.com>
2013-10-19 23:17:45 +0800

    Adding the current stable application
    This application includes the tab, the simple search and a splash screen

---
+++ src/megacebu/activities/FeaturedActivity.java
@@ -0,0 +1,14 @@
+package megacebu.activities;
+
+import megacebu.searchview.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeaturedActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_featured);
+    }
+}
